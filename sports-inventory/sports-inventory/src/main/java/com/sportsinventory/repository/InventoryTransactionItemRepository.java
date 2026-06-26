d0b4987006742412b9da0186aa437e7b0ce4f4eb
danila-prohor22 <prokhorenkov_danila@mail.ru>
2026-06-26 11:23:22 +0300

    Delete sports-inventory directory

--- sports-inventory/sports-inventory/src/main/java/com/sportsinventory/repository/InventoryTransactionItemRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sportsinventory.repository;
-
-import com.sportsinventory.entity.InventoryTransactionItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface InventoryTransactionItemRepository extends JpaRepository<InventoryTransactionItem, Long> {
-}
--- sports-inventory/src/main/java/com/sportsinventory/repository/StoredFileRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sportsinventory.repository;
-
-import com.sportsinventory.entity.StoredFile;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface StoredFileRepository extends JpaRepository<StoredFile, Long> {
-}
